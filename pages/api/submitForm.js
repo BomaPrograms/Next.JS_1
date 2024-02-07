2fd48d26c8bc8ff756c5653d6e211d750360a37b
BomaPrograms <BomaPrograms@users.noreply.github.com>
2024-02-07 15:15:10 +0000

    commit

--- pages/api/submitForm.js
+++ pages/api/submitForm.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   // Check if the request method is POST
   if (req.method === "POST") {
     // Send a response to the client
-    res.status(200).json({ message: "Form submitted successfully." }));
+    res.status(200).json({ message: "Form submitted successfully." });
   } else {
     // Send a failure response for non-POST requests
     res.status(405).json({ message: "Method not allowed." });
