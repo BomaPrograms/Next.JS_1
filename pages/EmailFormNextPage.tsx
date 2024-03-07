9ff39990c544ca232f1a38956da92f1e8f604159
BomaPrograms <BomaPrograms@users.noreply.github.com>
2024-03-07 10:25:50 +0000

    commit

---
+++ pages/EmailFormNextPage.tsx
@@ -0,0 +1,13 @@
+// EmailFormNextPage.tsx
+import React from "react";
+
+const EmailFormNextPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Email Form Page</h1>
+      {/* Add your email form here */}
+    </div>
+  );
+};
+
+export default EmailFormNextPage;
