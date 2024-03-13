963c2cda8079fa75832db1a5cd0a74ce7bbf4f40
BomaPrograms <BomaPrograms@users.noreply.github.com>
2024-03-13 10:43:29 +0000

    Update Next.js configuration and package.json

--- next.config.js
+++ next.config.js
@@ -4,12 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add an alias for the Prisma Client
-    config.resolve.alias["@prisma/client"] = require.resolve("@prisma/client");
-
-    return config;
-  },
-};
