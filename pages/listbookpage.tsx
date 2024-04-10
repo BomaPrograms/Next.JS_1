65f18bb624678b5bb8c75a4a375015e1315a2dae
BomaPrograms <BomaPrograms@users.noreply.github.com>
2024-04-10 12:40:34 +0100

    commit

--- pages/listbookpage.tsx
+++ pages/listbookpage.tsx
@@ -1,12 +1,14 @@
+// ListBooksPage.tsx
+
 import React from "react";
 import { api } from "../utils/api";
 
 const ListBooksPage: React.FC = () => {
   
-  const allBooks = api.post.getLatest.useQuery();
+  const Query = api.post.getLatest.useQuery();
   const { data: books = [] } = useQuery(["books.list"], {
     async resolve() {
-      return await api.allBooks.query.listBooks();
+      return await api.query.listBooks();
     },
   });
 
@@ -16,7 +18,7 @@ const ListBooksPage: React.FC = () => {
       {books.map((book) => (
         <div key={book.bookId}>
           <h3>{book.bookName}</h3>
-          {}
+          {/* Add more book details as needed */}
         </div>
       ))}
     </div>
